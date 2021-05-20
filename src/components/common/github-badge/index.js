910bbac3f7ce3183d1d8300e9adcd2364b2a892b
leandroercoli <leandroercoli@gmail.com>
2021-05-20 16:17:46 -0300

    fix: github badge

--- src/components/common/github-badge/index.js
+++ src/components/common/github-badge/index.js
@@ -5,7 +5,7 @@ function GithubBadge() {
   return (
     <div className="github-badge">
       <a
-        href={"https://github.com/leandroercoli/AxaTest"}
+        href={"https://gitfront.io/r/user-3342361/f62f8c70c1f04b5f80828d71d03382b100eafbd2/AxaTest/"}
         target="_blank"
         rel="noreferrer"
         aria-current="true"
